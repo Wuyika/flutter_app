813759fcc072189975316ff8f306fdec4fea7de8
Wuyika <wuyika.serge@gmail.com>
2021-08-10 13:19:28 +0100

    first flutter commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Demo'),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Text('Wuyika Serge')
+        )
+      )
+    );
+  }
+}
